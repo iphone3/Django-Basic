e23a9f57670a3c2181078bac7a7ee89594d69f40
Atom <313770142@qq.com>
2019-06-24 17:07:20 +0800

    标签for

--- 03-模板/DjangoTemplate/mt/views.py
+++ 03-模板/DjangoTemplate/mt/views.py
@@ -7,10 +7,38 @@ def index(request):
     # 用户名
     username = 'root'
     age = 18
+    names = [
+        '张三',
+        '李四',
+        '王五',
+        '赵六',
+        '田七'
+    ]
+
+    user_list = [
+        {
+            'username':'张三',
+            'score': 98,
+            'age': 18
+        },
+        {
+            'username': '李四',
+            'score': 78,
+            'age': 32
+        },
+        {
+            'username': '王五',
+            'score': 91,
+            'age': 13
+        },
+    ]
+
 
     context = {
         'name': username,
         'age': age,
+        'names':names,
+        'user_list': user_list
     }
 
     return render(request, 'index.html', context=context)
