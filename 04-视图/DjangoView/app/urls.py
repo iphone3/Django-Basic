ca236655daf507227c5bc13c6d197d41c854d932
Atom <313770142@qq.com>
2019-06-29 17:22:35 +0800

    0路径多参数传递

--- 04-视图/DjangoView/app/urls.py
+++ 04-视图/DjangoView/app/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     # http://127.0.0.1:8000/detail/4/
     # detail/4/
     url(r'^detail/(\d+)/$', views.detail, name='detail'),
+
+    # 3/1
+    # 3/2
+    # 3_1
+    # 3_2
+    url(r'^urlarg/(\d+)/(\d+)/(\w+)/$', views.urlarg, name='urlarg'),
+
+    url(r'^goods_detail/(?P<type_name>\w+)/(?P<goods_id>\d+)/$', views.goods_detail, name='goods_detail'),
 ]
--- 04-视图/DjangoView/app/views.py
+++ 04-视图/DjangoView/app/views.py
@@ -11,8 +11,9 @@ def detail(request, goodsId):
 
 
 def urlarg(request, a, b, c):
-    return HttpResponse('a:{} b:{} c:{}'.format(a,b,c))
+    return HttpResponse('a={}  b={}  c={}'.format(a,b,c))
 
 
-def goods_detail(request, type_name, goods_id):
-    return HttpResponse('商品分类: {}  详情：{}'.format(type_name, goods_id))
+
+def goods_detail(request, goods_id, type_name):
+    return HttpResponse(' {} 分类 商品详情: {}'.format(type_name, goods_id))
