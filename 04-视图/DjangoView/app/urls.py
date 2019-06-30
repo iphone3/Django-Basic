c4830ea74014844b5ce42969d6e94046f0f72387
Atom <313770142@qq.com>
2019-06-30 10:42:34 +0800

    重定向和反向解析

--- 04-视图/DjangoView/app/urls.py
+++ 04-视图/DjangoView/app/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import url
 
 from app import views
 
+
+# 2xx 成功类
+# 3xx 重定向
+# 4xx 客户端错误
+# 5xx 服务端错误
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 
@@ -17,4 +23,7 @@ urlpatterns = [
     url(r'^urlarg/(\d+)/(\d+)/(\w+)/$', views.urlarg, name='urlarg'),
 
     url(r'^goods_detail/(?P<type_name>\w+)/(?P<goods_id>\d+)/$', views.goods_detail, name='goods_detail'),
+
+    url(r'^login_html/$', views.login_html, name='login_html'),
+    url(r'^login/$', views.login, name='login'),
 ]
--- 04-视图/DjangoView/app/views.py
+++ 04-视图/DjangoView/app/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def index(request):
+
     return render(request, 'index.html')
 
 
@@ -17,3 +19,15 @@ def urlarg(request, a, b, c):
 
 def goods_detail(request, goods_id, type_name):
     return HttpResponse(' {} 分类 商品详情: {}'.format(type_name, goods_id))
+
+
+def login_html(request):
+    return render(request, 'login.html')
+
+
+def login(request):
+    # 登录成功后，重定向到首页
+    # return redirect('/')
+    return redirect('app:index')
+    # return redirect('app:urlarg', 1,5,10)
+    # return redirect('app:goods_detail', goods_id=10, type_name='服装类')
