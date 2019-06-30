3914ad331b8bc8e93e128eaf0fa3f7989a3cd3f1
Atom <313770142@qq.com>
2019-06-30 11:49:44 +0800

    错误页面定制

--- 04-视图/DjangoView/DjangoView/settings.py
+++ 04-视图/DjangoView/DjangoView/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qksr%@9v*bwmzs5ml^u)v=4*6s!(v$m_p_lzaj$497+(6hgz_p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- 04-视图/DjangoView/app/views.py
+++ 04-视图/DjangoView/app/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-
     return render(request, 'index.html')
 
 
